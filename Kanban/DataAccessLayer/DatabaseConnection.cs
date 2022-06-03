fc5d0bf6232084675d8fd470e95938facb1541f2
W <w___B@wp.pl>
2022-06-03 23:34:39 +0200

    add: basic database connection class (wip)

---
+++ Kanban/DataAccessLayer/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Kanban.DataAccessLayer
+{
+    internal class DatabaseConnection
+    {
+        private readonly MySqlConnectionStringBuilder connectionStringBuilder = new();
+        private static DatabaseConnection? instance;
+
+        public static DatabaseConnection Instance => instance ??= new();
+
+        public MySqlConnection Connection => new(connectionStringBuilder.ToString());
+
+        private DatabaseConnection()
+        {
+            connectionStringBuilder.UserID = Properties.Settings.Default.userID;
+            connectionStringBuilder.Server = Properties.Settings.Default.server;
+            connectionStringBuilder.Database = Properties.Settings.Default.database;
+        }
+    }
+}
