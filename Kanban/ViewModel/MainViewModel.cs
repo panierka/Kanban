1b204b70ea5ef5a9d4b90fa0b431b5ec649e80be
jdylik <jan.karol.dylik@gmail.com>
2022-06-19 11:43:34 +0200

    Drag and Drop p1

---
+++ Kanban/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using Kanban.ViewModel.Base;
+using System.Windows;
+
+namespace Kanban
+{
+    internal class MainViewModel:BaseViewModel
+    {
+        private RelayCommand mousemove = null;
+        public void Student(object sender)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            if (rectangle != null && System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(rectangle,
+                                     rectangle.Fill.ToString(),
+                                     DragDropEffects.Copy);
+            }
+            Console.WriteLine("std");
+        }
+        public RelayCommand p_MouseMove
+        {
+            get
+            {
+                if (mousemove == null)
+                {
+                    mousemove = new RelayCommand(Student, argument => true);
+                }
+                return mousemove;
+            }
+        }
+    }
+}
